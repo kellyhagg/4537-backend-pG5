e4f8652b1d33614082c27381a70f5ee714dcbefa
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2024-04-04 19:05:48 -0700

    Remove apiCallsCount from user schema

--- src/user.js
+++ src/user.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  apiCallsCount: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('User', userSchema);
