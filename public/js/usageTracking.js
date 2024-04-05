4f08b181a017919151d04b765d096621cc3c8523
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2024-04-04 22:10:46 -0700

    Add an AJAX call to populate endpoint stats table

--- public/js/usageTracking.js
+++ public/js/usageTracking.js
@@ -1,5 +1,6 @@
 // File made with the assistance of Chat-GPT
 $(document).ready(function () {
+    // AJAX call for usage-tracking
     $.ajax({
         url: '/auth/user-api-calls',
         type: 'GET',
@@ -17,6 +18,26 @@ $(document).ready(function () {
         },
         error: function (error) {
             console.log('Error fetching user API calls:', error);
+        }
+    });
+
+    // AJAX call for method-tracking
+    $.ajax({
+        url: '/auth/api-stats', // Update with the correct path to your backend endpoint
+        type: 'GET',
+        success: function (apiStats) {
+            let tableRows = '';
+            apiStats.forEach(function (stat) {
+                tableRows += '<tr>';
+                tableRows += '<td>' + stat.method + '</td>';
+                tableRows += '<td>' + stat.endpoint + '</td>';
+                tableRows += '<td>' + stat.requestCount + '</td>';
+                tableRows += '</tr>';
+            });
+            $('#methodTrackingTable tbody').html(tableRows);
+        },
+        error: function (error) {
+            console.log('Error fetching API stats:', error);
         }
     });
 });
